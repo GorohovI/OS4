8ebe71cd49b2a653a61900db0f638d88655d43f5
GorohovI <gorohovi@gmail.com>
2010-04-26 10:12:16 +0600

    first - done second - only exceptions

--- forksig.c
+++ forksig.c
@@ -3,13 +3,13 @@
 #include <sys/ipc.h>
 #include <unistd.h>
 #include <signal.h>
+#include <stdio.h>
 
 void sigevent(int k)
 {
   printf("PID %i hear %i\n",getpid(),k);
 }
-
-int main (int argс, char* argv[])
+int main(int argc, char * argv[])
 { //создаем сою группу процессов
   struct sigaction new,old;
   new.sa_handler=&sigevent;
@@ -18,11 +18,10 @@ int main (int argс, char* argv[])
   sigaction(SIGUSR1,&new,&old);
   if(setpgid(0,0) != 0)
     perror ("can't set new group");
-  printf("GID: %i\nPID: %i\n",getgpid(),getpid()); 
-  int cldcnt,i;
+  printf("GID: %i\nPID: %i\n",getpgrp(),getpid()); 
+  int cldcnt=3,i;
   //узнаем число потомков
-  if(argc < 2) cldcnt=5;
-  else cldcnt=atoi(argv[1]);
+  if(argc > 1) cldcnt=atoi(argv[1]);
   pid_t* cld_pids=calloc(cldcnt,sizeof(pid_t));
   //создаем потомков
   for(i=0;i<cldcnt;++i)
--- fp.c
+++ fp.c
@@ -12,15 +12,16 @@ int main ()
   //execlp("ls",(char *)0); 
   int i,len,arg3,arg2;
   char* buf = malloc(BUFSIZE);
-printf("type eXiT to end progran");
+printf("type quit to end progran");
 while(1){
+  setjmp();
   printf(">");
   fgets(buf,BUFSIZE-1,stdin);
-  if(buf[0]=='e' && buf[1]=='X' && buf[2]=='i' && buf[3]=='T')exit(0);
+  if(buf[0]=='q' && buf[1]=='u' && buf[2]=='i' && buf[3]=='t')exit(0);
   len=strlen(buf);
   for(arg3=len-1;arg3>=0 && buf[arg3]!=';';--arg3);
   buf[len-1]=0;
-  if(arg3<=0)perror("invalid input\n");
+  if(arg3<=0)//perror("invalid input\n");
   buf[arg3++]=0;
   for(arg2=arg3-1;arg2>=0 && buf[arg2]!=';';--arg2);
   if(arg2<=0)perror("invalid input\n");
@@ -41,23 +42,31 @@ printf("%s_\n",buf);
 	  if(dup2(p1[1],1)==-1)perror("can't dup\n");
 	  if(dup2(p2[1],2)==-1)perror("can't dup\n");
 	  execlp(buf,(char *)0);
+	printf("exec error\n");
 	exit(0);
 	}
 	if(i==1){
 	  if(dup2(p1[0],0)==-1)perror("can't dup on exit pipe\n");
 printf("%s_\n",buf+arg2);
 	  execlp(buf+arg2,(char *)0);
+	printf("exec error\n");
 	exit(0);
 	}
 	if(dup2(p2[0],0)==-1)perror("can't dup on exit pipe\n");
 printf("%s_\n",buf+arg3);
 	execlp(buf+arg3,(char *)0);
+	printf("exec error\n");
 exit(0);
     }else {
       perror("can't fork\n");
     }
   //  завершаем работу
   int status,pid;
+  close(p1[0]);
+  close(p1[1]);
+  close(p2[0]);
+  close(p2[1]);
+   
   for(i=0;i<CLDCNT;++i){
   pid=wait(&status);
   printf("process %i done with code %i\n",pid,status);}
